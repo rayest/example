57d9bc4cb5da0466986787bc8066d84f8c199757
Rayest <yi@rayest.cn>
2016-05-15 18:37:02 +0800

    Simple Springmvc Hello World

---
+++ src/main/java/controller/InputProductController.java
@@ -0,0 +1,26 @@
+package controller;
+
+/**
+ * Created by Administrator on 2016/5/15 0015.
+ */
+        import javax.servlet.http.HttpServletRequest;
+        import javax.servlet.http.HttpServletResponse;
+
+        import org.apache.commons.logging.Log;
+        import org.apache.commons.logging.LogFactory;
+        import org.springframework.web.servlet.ModelAndView;
+        import org.springframework.web.servlet.mvc.Controller;
+
+public class InputProductController implements Controller {
+
+    private static final Log logger = LogFactory
+            .getLog(InputProductController.class);
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request,
+                                      HttpServletResponse response) throws Exception {
+        logger.info("InputProductController called");
+        return new ModelAndView("/WEB-INF/jsp/ProductForm.jsp");
+    }
+
+}
